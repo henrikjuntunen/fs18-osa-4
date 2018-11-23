618e92c430092a1a5df944e691829a050e0f51b3
Henrik Juntunen <henrik.juntunen@pp.inet.fi>
2018-11-23 15:26:34 +0200

    Tehtävä 4.2 init

---
+++ tests/list_helper.test.js
@@ -0,0 +1,38 @@
+const palindrom = require('../utils/list_helper').palindrom
+
+describe('palindrom', () => {
+
+    test('palindrom of a', () => {
+        const result = palindrom('a')
+        expect(result).toBe('a')
+    })
+
+    test('palindrom of react', () => {
+        const result = palindrom('react')
+        expect(result).toBe('tcaer')
+    })
+
+    test('palindrom of saippuakauppias', () => {
+        const result = palindrom('saippuakauppias')
+        expect(result).toBe('saippuakauppias')
+    })
+
+})
+
+const average = require('../utils/list_helper').average
+
+describe('average', () => {
+
+    test('of one value is the value itself', () => {
+      expect(average([1])).toBe(1)
+    })
+  
+    test('of many is calculated right', () => {
+      expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
+    })
+  
+    test('of empty array is zero', () => {
+      expect(average([])).toBe(0)
+    })
+  
+})
